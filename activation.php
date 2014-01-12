51e24f68beda213550f2999b4cd9d7db05988656
Roy Kuijper <roy97@casema.nl>
2014-01-12 20:47:36 +0100

    We hebben de activation.php gemaakt

---
+++ activation.php
@@ -0,0 +1,18 @@
+<p>welkom op de site. uw account wordt geactiveerd nadat u een nieuw wachtwoord heeft gekozen</p>
+<from action='' method='post'>
+	<table>
+		<tr>
+			<td>nieuw wachtwoord(maximaal 12 tekens)</td>
+			<td><input type='text' name='password' size='12' maxlength='12' /></td>
+		</tr>
+		<tr>
+			<td>nieuw wachtwoord (check)</td>
+			<td><input type='text' name='password-check' size='12' maxlength='12' /></td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td><input type'submit' value=''</td>
+		</tr>
+		
+	</table>
+</form>
